e71e26ebd23c1eb7f80167ca291fe60cf1d49902
徳永 潮 <tokunaga.u@tsunagu-grp.jp>
2024-11-13 22:31:50 +0900

    add: 個別投稿ページの追加

---
+++ app/public/wp-content/themes/mytheme/single.php
@@ -0,0 +1,13 @@
+<!-- 投稿の個別ページ -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <?php get_header(); ?>
+  <title>Document</title>
+</head>
+
+<body>
+  <p>aaaa</p>
+  <?php get_footer(); ?>
+</body>
+</html>
