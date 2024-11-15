7a99fe6258b00da9efb5160e94b0f907b5a237da
徳永 潮 <tokunaga.u@tsunagu-grp.jp>
2024-11-15 14:45:39 +0900

    add: TOPに投稿一覧を追加

--- app/public/wp-content/themes/mytheme/front-page.php
+++ app/public/wp-content/themes/mytheme/front-page.php
@@ -14,21 +14,26 @@
   <?php $args = array('post_type' => 'post'); ?> <!-- post（投稿）のセット -->
   <?php $query = new WP_Query($args) ?> <!-- クエリの作成 -->
 
-  <?php if($query->have_posts()): ?>
-    <?php while($query->have_posts()): $query->the_post(); ?>
-      
-      <a href="<?php the_permalink(); ?>">
-        <h2><?php the_title(); ?></h2>
-        <p><?php the_excerpt(); ?></p>
-        <p><?php the_time(get_option('date_format')); ?></p>
-      </a>
-      <?php the_author(); ?>
-      <hr>
-    <? endwhile; ?>
-  <?php else: ?>
-    <p>投稿がありません</p>
-  <?php endif; ?>
-  
+  <main>
+    <div class="notice">
+      <h2 class="notice__ttl">お知らせ</h2>
+      <div class="notice__lists">
+        <?php $args = array('post_type' => 'post'); ?> <!-- post（投稿）のセット -->
+        <?php $query = new WP_Query($args) ?> <!-- クエリの作成 -->
+        <?php if($query->have_posts()): ?>
+          <?php while($query->have_posts()): $query->the_post(); ?>
+            <a class="notice__lists__item" href="<?php the_permalink(); ?>">
+              <p class="notice__lists__item__txt--date"><?php the_time('Y年m月d日'); ?></p>
+              <p class="notice__lists__item__txt--ttl"><?php the_title(); ?></p>
+            </a>
+          <? endwhile; ?>
+        <?php else: ?>
+          <p>投稿がありません</p>
+        <?php endif; ?>
+      </div>
+      <button class="notice__btn" onclick="location.href=''" type="button">お知らせ一覧</button>
+    </div>
+  </main>
 
   <?php get_footer(); ?>
 </body>
