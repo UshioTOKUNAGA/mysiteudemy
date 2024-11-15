5042dee8e6e7a00026b87d88e68d44b2d427b106
徳永 潮 <tokunaga.u@tsunagu-grp.jp>
2024-11-15 14:08:57 +0900

    fix: functions.phpでのCSS読み込みを修正

--- app/public/wp-content/themes/mytheme/function.php
+++
@@ -1,6 +0,0 @@
-<?php
-add_action( 'wp_enqueue_scripts', function(){
-  if( is_front_page() ) {
-    wp_enqueue_style( 'front-page', get_template_directory_uri() . '/css/front-page.css');
-  }
-} );
---
+++ app/public/wp-content/themes/mytheme/functions.php
@@ -0,0 +1,7 @@
+<?php
+function read_css(){
+  if( is_front_page() ) {
+    wp_enqueue_style( 'front-page-style', get_template_directory_uri() . '/css/front-page.css');
+  }
+}
+add_action('wp_enqueue_scripts', 'read_css');
