aa15710459a309950faae459e1393805d5c05fbd
徳永 潮 <tokunaga.u@tsunagu-grp.jp>
2024-11-15 14:46:32 +0900

    add: footerの追加

--- app/public/wp-content/themes/mytheme/front-page.php
+++ app/public/wp-content/themes/mytheme/front-page.php
@@ -35,6 +35,37 @@
     </div>
   </main>
 
+  <footer>
+    <div class="footer__inner1200">
+      <div>
+        <h1 class="footer__logo">会社ロゴ</h1>
+        <p class="footer__adress">
+          〒104-0061<br>
+          東京都中央区銀座7-3-5<br>
+          ヒューリック銀座7丁目ビル7・8階（受付8階）
+        </p>
+        <ul class="footer__snsLinks">
+          <li><img src="<?php echo get_template_directory_uri(); ?>/img/instagram_icon.svg" alt=""></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/img/facebook_icon.svg" alt=""></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/img/youtube_icon.svg" alt=""></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/img/x_icon.svg" alt=""></li>
+        </ul>
+      </div>
+      <div>
+        <nav>
+          <ul class="footer__menu">
+            <li class="footer__menu__item"><a href="">会社概要</a></li>
+            <li class="footer__menu__item"><a href="">お知らせ</a></li>
+            <li class="footer__menu__item"><a href="">採用情報</a></li>
+            <li class="footer__menu__item"><a href="">お問い合わせ</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <p class="footer__copyRight">©2024 SampleCompany</p>
+
+  </footer>
+
   <?php get_footer(); ?>
 </body>
 </html>
