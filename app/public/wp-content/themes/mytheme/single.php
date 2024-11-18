4f27c4b2f043ad3232788575bd408b5e1b267eb3
徳永 潮 <tokunaga.u@tsunagu-grp.jp>
2024-11-18 09:11:29 +0900

    add: 個別投稿ページの作成

--- app/public/wp-content/themes/mytheme/single.php
+++ app/public/wp-content/themes/mytheme/single.php
@@ -2,11 +2,27 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <?php get_header(); ?>
-</head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php the_title(); ?></title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
 
+  <?php wp_head(); ?>
+</head>
 <body>
-  <p>aaaa</p>
+  <?php if(have_posts()): ?>
+    <?php while(have_posts()) : the_post(); ?>
+    <h1><?php the_title(); ?></h1>
+    <p><?php the_time('Y年m月d日'); ?></p>
+
+    <!-- 投稿の中身の表示 -->
+    <div>
+      <?php the_content(); ?>
+    </div>
+    <?php endwhile; ?>
+  
+  <?php endif; ?>
+
   <?php get_footer(); ?>
 </body>
-</html>
+</html> 
