a81ca959059e99914d77a4036d91441004870f3f
徳永 潮 <tokunaga.u@tsunagu-grp.jp>
2025-02-17 09:02:37 +0900

    fix: 会社概要ページのレイアウト調整

--- app/public/wp-content/themes/mytheme/functions.php
+++ app/public/wp-content/themes/mytheme/functions.php
@@ -1,19 +1,24 @@
 <?php
-function read_css(){
-  if( is_front_page() ) {
-    wp_enqueue_style( 'front-page-style', get_template_directory_uri() . '/css/front-page.css');
+function read_css()
+{
+  if (is_front_page()) {
+    wp_enqueue_style('front-page-style', get_template_directory_uri() . '/css/front-page.css');
+  }
+  if (is_page('company')) {
+    wp_enqueue_style('company', get_template_directory_uri() . '/css/company.css');
   }
 }
 add_action('wp_enqueue_scripts', 'read_css');
 
 //投稿でアイキャッチ画像の設定を表示
-add_action('init', function(){
+add_action('init', function () {
   add_theme_support('post-thumbnails');
 });
 
 // アイキャッチ画像がなければ、標準画像を取得する
-function get_eyecatch_with_default(){
-  if(has_post_thumbnail()):
+function get_eyecatch_with_default()
+{
+  if (has_post_thumbnail()):
     $id = get_post_thumbnail_id();
     $img = wp_get_attachment_image_src($id, 'large');
   else:
--- app/public/wp-content/themes/mytheme/page-company.php
+++ app/public/wp-content/themes/mytheme/page-company.php
@@ -1,27 +1,34 @@
 <!-- 固定ページ-会社概要 -->
 <?php
-  /*
+/*
   Template Name: 会社概要
   */
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php the_title(); ?></title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css" />
 
   <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
   <?php wp_body_open(); ?>
 
-  <div class="wrap">
+  <main>
     <h1 class="ttl"><?php the_title(); ?></h1>
-    <?php the_content(); ?>
-  </div>
+    <div class="wrap">
+      <div class="inner">
+        <?php the_content(); ?>
+      </div>
+    </div>
+  </main>
 
   <?php wp_footer(); ?>
 </body>
+
 </html>
