072ab41963b619f0c669e51659153823e2644114
徳永 潮 <tokunaga.u@tsunagu-grp.jp>
2025-03-21 01:51:17 +0900

    fix: フッターのコンポーネント化

--- app/public/wp-content/themes/mytheme/front-page.php
+++ app/public/wp-content/themes/mytheme/front-page.php
@@ -19,7 +19,7 @@
     <section class="notice">
       <h2 class="notice__ttl">お知らせ</h2>
       <div class="notice__lists">
-        <?php $args = array('post_type' => 'post'); ?> <!-- post（投稿）のセット -->
+        <?php $args = array('post_type' => 'news'); ?> <!-- post（投稿）のセット -->
         <?php $query = new WP_Query($args) ?> <!-- クエリの作成 -->
         <?php if($query->have_posts()): ?>
           <?php while($query->have_posts()): $query->the_post(); ?>
@@ -36,36 +36,7 @@
     </section>
   </main>
 
-  <footer>
-    <div class="footer__inner1200">
-      <div>
-        <h1 class="footer__logo">会社ロゴ</h1>
-        <p class="footer__adress">
-          〒104-0061<br>
-          東京都中央区銀座7-3-5<br>
-          ヒューリック銀座7丁目ビル7・8階（受付8階）
-        </p>
-        <ul class="footer__snsLinks">
-          <li><img src="<?php echo get_template_directory_uri(); ?>/img/instagram_icon.svg" alt=""></li>
-          <li><img src="<?php echo get_template_directory_uri(); ?>/img/facebook_icon.svg" alt=""></li>
-          <li><img src="<?php echo get_template_directory_uri(); ?>/img/youtube_icon.svg" alt=""></li>
-          <li><img src="<?php echo get_template_directory_uri(); ?>/img/x_icon.svg" alt=""></li>
-        </ul>
-      </div>
-      <div>
-        <nav>
-          <ul class="footer__menu">
-            <li class="footer__menu__item"><a href="">会社概要</a></li>
-            <li class="footer__menu__item"><a href="">お知らせ</a></li>
-            <li class="footer__menu__item"><a href="">採用情報</a></li>
-            <li class="footer__menu__item"><a href="">お問い合わせ</a></li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-    <p class="footer__copyRight">©2024 SampleCompany</p>
-
-  </footer>
+  <?php wp_footer(); ?>
 
   <?php get_footer(); ?>
 </body>
