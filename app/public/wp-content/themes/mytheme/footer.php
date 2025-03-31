d88ad8adb52307607d5889bb710a3b641aa6b83e
徳永 潮 <tokunaga.u@tsunagu-grp.jp>
2025-03-31 12:26:14 +0900

    fix: フッターのリンク先の設定

--- app/public/wp-content/themes/mytheme/footer.php
+++ app/public/wp-content/themes/mytheme/footer.php
@@ -8,19 +8,19 @@
         ヒューリック銀座7丁目ビル7・8階（受付8階）
       </p>
       <ul class="footer__snsLinks">
-        <li><img src="<?php echo get_template_directory_uri(); ?>/img/instagram_icon.svg" alt=""></li>
-        <li><img src="<?php echo get_template_directory_uri(); ?>/img/facebook_icon.svg" alt=""></li>
-        <li><img src="<?php echo get_template_directory_uri(); ?>/img/youtube_icon.svg" alt=""></li>
-        <li><img src="<?php echo get_template_directory_uri(); ?>/img/x_icon.svg" alt=""></li>
+        <a href="https://www.instagram.com/?locale=ja_JP"><li><img src="<?php echo get_template_directory_uri(); ?>/img/instagram_icon.svg" alt=""></li></a>
+        <a href="https://www.facebook.com/?locale=ja_JP"><li><img src="<?php echo get_template_directory_uri(); ?>/img/facebook_icon.svg" alt=""></li></a>
+        <a href="https://www.youtube.com/?app=desktop&hl=ja"><li><img src="<?php echo get_template_directory_uri(); ?>/img/youtube_icon.svg" alt=""></li></a>
+        <a href="https://x.com/"><li><img src="<?php echo get_template_directory_uri(); ?>/img/x_icon.svg" alt=""></li></a>
       </ul>
     </div>
     <div>
       <nav>
         <ul class="footer__menu">
-          <li class="footer__menu__item"><a href="">会社概要</a></li>
-          <li class="footer__menu__item"><a href="">お知らせ</a></li>
-          <li class="footer__menu__item"><a href="">採用情報</a></li>
-          <li class="footer__menu__item"><a href="">お問い合わせ</a></li>
+          <li class="footer__menu__item"><a href="<?php echo site_url('/company') ?>">会社概要</a></li>
+          <li class="footer__menu__item"><a href="<?php echo site_url('/news') ?>">お知らせ</a></li>
+          <li class="footer__menu__item"><a href="<?php echo site_url('/recruit') ?>">採用情報</a></li>
+          <li class="footer__menu__item"><a href="<?php echo site_url('/contact') ?>">お問い合わせ</a></li>
         </ul>
       </nav>
     </div>
